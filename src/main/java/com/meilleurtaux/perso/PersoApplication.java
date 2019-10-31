2fd7a4d886d10f0c49a85a269d7d007782845fd9
Stéphane Gallois <sgallois@meilleurtaux.com>
2019-10-31 14:35:24 +0100

    initial comit

---
+++ src/main/java/com/meilleurtaux/perso/PersoApplication.java
@@ -0,0 +1,13 @@
+package com.meilleurtaux.perso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meilleurtaux/perso/PersoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meilleurtaux.perso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
